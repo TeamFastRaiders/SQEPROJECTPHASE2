c18c1e62fd9e0523623d12af6a1adc5d10e4cd38
Sohaib Ali Khan <84563883+sohaib1268@users.noreply.github.com>
2022-10-24 01:15:46 +0500

    Automation Environment Working
    Automation environment set up complete , test case 1 executed succesfully

--- Selenium-Cucumber-TestNG-Maven/src/test/java/StepDefinitions/CatalogBrowsingSteps.java
+++ Selenium-Cucumber-TestNG-Maven/src/test/java/StepDefinitions/CatalogBrowsingSteps.java
@@ -1,18 +1,35 @@
 package StepDefinitions;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class CatalogBrowsingSteps {
+	
+	WebDriver driver = null;
 	@Given("that user is in a catalog")
 	public void that_user_is_in_a_catalog() {
-	    System.out.println("I am in the Catalog");
+		
+		System.out.println("I am in the Catalog");
+		String projectPath = System.getProperty("user.dir");
+	    System.setProperty("webdriver.chrome.driver",projectPath+"/src/test/resources/drivers/chromedriver.exe");
+	    driver = new ChromeDriver();
+	    driver.navigate().to("https://www.shoecarnival.com/mens/athletics_and_sneakers/running_shoes");
+	    
 	}
 
 	@When("the user clicks on the Single Arrow Forward")
 	public void the_user_clicks_on_the_Single_Arrow_Forward() {
+	   //driver.findElement(By.className("ais-Pagination-link")).sendKeys(Keys.ENTER);
+	   driver.findElement(By.xpath("/html[1]/body[1]/div[1]/main[1]/div[1]/div[2]/div[4]/div[2]/div[4]/div[1]/div[1]/div[2]/div[2]/div[1]/ul[1]/li[4]/a[1]")).sendKeys(Keys.ENTER);
 	   System.out.println("Single Arrow Forward has been clicked");
 	}
 
